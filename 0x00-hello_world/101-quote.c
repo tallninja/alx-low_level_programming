93cd56ae62c4e2d23781e42ec71244b4722c6d76
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-10 20:12:00 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
 
-int main(void
+/**
+ * prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error.
+ */
+int main(void)
 {
   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
   return 1;
