f992dd9aef00399a6569304bfe150100a78f6be4
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-11 09:51:47 +0300

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase excepr q and e
+* Return: 0 after successful execution
+*/
+int main(void)
+{
+	for (char chr = 'a'; chr <= 'z'; chr++)
+	{
+		if (chr != 'q' && chr != 'e')
+		{
+			putchar(chr);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
