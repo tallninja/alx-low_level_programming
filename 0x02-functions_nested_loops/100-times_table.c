08c80d034973cf1abbc9adc110a04bdf033963a3
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-15 15:36:26 +0300

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,14 +3,15 @@
 
 /**
 * print_times_table - prints all natural numbers from n to 98
+* @n: limit
 * Return: nothing
 */
 void print_times_table(int n)
 {
 	int row, col;
-	
+
 	if ((n <= 15) && (n >= 0))
-	{	
+	{
 		for (row = 0; row <= n; row++)
 		{
 			for (col = 0; col <= n; col++)
@@ -21,15 +22,14 @@ void print_times_table(int n)
 					printf("  ");
 				else if (product < 100 && col > 0)
 					printf(" ");
-				
+
 				printf("%d", product);
-				
+
 				if (col < n)
 				{
 					printf(", ");
 				}
 			}
-			
 			printf("\n");
 		}
 	}
