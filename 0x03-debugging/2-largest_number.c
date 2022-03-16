38e4000f35e780be19a6ac5f356cee4e76932c31
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-16 10:01:47 +0300

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,23 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
-	{
+	if (a == b && a > c)
+		largest = a
+;
+	else if (a == c && a > b)
 		largest = a;
-	}
-	else if (b > a && b > c)
-	{
+
+	else if (b == c && b > a)
 		largest = b;
-	}
+
+	else if (a > b && a > c)
+		largest = a;
+
+	else if (b > a && b > c)
+		largest = b;
+
 	else
-	{
 		largest = c;
-	}
 
 	return (largest);
 }
