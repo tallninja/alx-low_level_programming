5fb918928fd1c81f890ee423b417f141d301fc1f
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-17 12:40:31 +0300

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer
+* @n: integer to be printed
+* Return: nothing
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	if (n / 10)
+	{
+		/* Recursion */
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
