d9ab05ea5ec289f2e8a53ef3e2ddc0ea3370f41e
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-28 15:28:27 +0300

    101-crackme_password

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
-* _strchr - locates a character in a string
-* @s: string to search through
-* @c: character we are looking for
-* Return: pointer to the first occurence of c
-*/
+ * _strchr - locates a character in a string
+ * @s: string to search through
+ * @c: character we are looking for
+ * Return: pointer to the first occurence of c
+ */
 
 char *_strchr(char *s, char c)
 {
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,15 +1,15 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
-* _strpbrk - searches a string for
-* any of a set of bytes
-* @s: the srting to be searched
-* @accept: occurence string
-* Return: pointer to the byte in s that matches
-* one of the bytes in accept, or NULL if no byte
-* is found
-*/
+ * _strpbrk - searches a string for
+ * any of a set of bytes
+ * @s: the srting to be searched
+ * @accept: occurence string
+ * Return: pointer to the byte in s that matches
+ * one of the bytes in accept, or NULL if no byte
+ * is found
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
