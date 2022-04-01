d0e93a0097c5839453023e20437ff53e003e6103
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-01 19:09:58 +0300

    3-mul

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - prints the number of command line
-* arguments passed to it
-* @argc: number of command line arguments
-* @argv: array containing the command line arguments
-*/
+ * main - prints the number of command line
+ * arguments passed to it
+ * @argc: number of command line arguments
+ * @argv: array containing the command line arguments
+ * Return: 0 if executed successfully
+ */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints all the command line
- * arguments passed to it
+ * main - multiplies two numbers
  * @argc: number of command line arguments
  * @argv: array containing the command line arguments
  * Return: 0 if executed successfully
@@ -10,10 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-
+	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	return (0);
 }
