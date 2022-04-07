11b5d12e3911b74b51ae4ecee60f6362038f228d
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-07 20:24:04 +0300

    100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,6 +24,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 		if (new_ptr == NULL)
 			return (NULL);
+
+		return (new_ptr);
 	}
 
 	if (new_size == 0 && ptr != NULL)
