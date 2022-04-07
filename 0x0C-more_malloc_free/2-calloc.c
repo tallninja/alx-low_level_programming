9266af05fb8b4c744d8bf445793694f27aecfd2c
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-07 19:32:51 +0300

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	for (i = 0; i < nmemb * size; i++)
-		*((char *)pointer + i) = 0;
+		*((char *)arr + i) = 0;
 
 	return (arr);
 }
