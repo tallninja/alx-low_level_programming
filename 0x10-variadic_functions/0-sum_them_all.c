bbac315a88380186bb712950d369b6f995c47872
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-14 11:02:57 +0300

    0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,19 +11,17 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-	va_list arg_list;
+	va_list arg_ptr;
 
 	if (n == 0)
 		return (0);
 
-	va_start(arg_list, n);
+	va_start(arg_ptr, n);
 
 	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(arg_list, int);
-	}
+		sum += va_arg(arg_ptr, int);
 
-	va_end(arg_list);
+	va_end(arg_ptr);
 
 	return (sum);
 }
