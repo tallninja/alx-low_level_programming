a850e5abec4299d428916421ee68ee2542c969e5
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-21 13:45:51 +0300

    101-hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,17 +3,16 @@ extern printf	; tells the assembler that this symbol exists outside of this file
 global main	; needed for gcc and it’s the entry point for libc
 
 section .data
-    message	db	"Hello, Holberton", 10, 0 ; the string we want to print followed by a new line and null terminator
-    fmt		db	"%s", 0 ; format specifier
+    message	db	"Hello, Holberton", 10, 0	; the string we want to print followed by a new line and null terminator
+    fmt		db	"%s", 0				; format specifier
 
 section .text
     main:
-        ; Create a stack-frame, re-aligning the stack to 16-byte alignment before calls
-        push	rbp
+        push	rbp			; Create a stack-frame, re-aligning the stack to 16-byte alignment before calls
         mov	rdi, fmt
         mov	rsi, message
         mov	rax, 0
-        call	printf wrt ..plt ; call printf with relation to procedure linkage table
-        pop	rbp ; Pop stack
-        mov	rax, 0   ; Exit code 0
+        call	printf wrt ..plt 	; call printf with relation to procedure linkage table
+        pop	rbp 			; Pop stack
+        mov	rax, 0   		; Exit code 0
         ret
