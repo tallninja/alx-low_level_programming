a89bb6eb1dbfe13ff69b91b3dc332154abf70ad8
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-21 10:48:14 +0300

    2-add_node

---
+++ 0x12-singly_linked_lists/_strlen.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @str: the string;
+ * Return: length of the string
+ */
+
+int _strlen(char *str)
+{
+	int i, len = 0;
+
+	for (i = 0; str[i] != '\0', i++)
+		len++
+
+	return (len);
+}
