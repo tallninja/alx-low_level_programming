6b1e889bec09c8667720ab7b382bf4101bf9eee4
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-21 12:11:55 +0300

    4-free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * free_node - frees space accupied by linked list
+ * @node: node
+ */
+
+void free_node(list_t *node)
+{
+	free(node->str);
+	free(node);
+}
+
+/**
+ * free_list - frees a linked list
+ * @head: pointer to the head of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *next_node;
+
+	while (head->next != NULL)
+	{
+		next_node = head->next;
+		free_node(head);
+		head = next_node;
+	}
+
+	free_node(head);
+}
