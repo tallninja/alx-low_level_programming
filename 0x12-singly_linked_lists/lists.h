fb09fbd28d8cd49e0d2d5fe302d415841b5f95b7
Ernest Wambua <ernestwambua2@gmail.com>
2022-04-21 09:35:21 +0300

    0-print_list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -9,11 +11,14 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif
