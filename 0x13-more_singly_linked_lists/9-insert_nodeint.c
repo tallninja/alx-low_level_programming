0037ac4da37d979f5075e027395a7cb89f830250
tallninja <tallninja123@gmail.com>
2022-04-26 11:39:01 +0300

    9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements
- * in a linked list
- * @h: linked list
- * Return: number of elements in h
- */
-
-size_t listint_len(const listint_t *h)
-{
-	unsigned int num_elements = 0;
-
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
-	{
-		h = h->next;
-		num_elements++;
-	}
-
-	return (num_elements);
-}
-
-/**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: list
  * @idx: index
@@ -34,14 +11,9 @@ size_t listint_len(const listint_t *h)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *node;
-	unsigned int i = 0;
-	size_t len;
+	unsigned int i = 1;
 
 	if (*head == NULL)
-		return (NULL);
-
-	len = listint_len(*head);
-	if (idx > len)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
@@ -49,21 +21,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
 	node = *head;
-	while (node != NULL && i < idx - 1)
-		node = node->next, i++;
+	while (i < idx)
+	{
+		node = node->next;
+		if (node == NULL)
+			return (NULL);
+		i++;
+	}
 
-	if (node != NULL)
-	{
-		new_node->next = node->next;
-		node->next = new_node;
-	}
-	else
-	{
-		new_node->next = NULL;
-		node->next = new_node;
-	}
+	new_node->next = node->next;
+	node->next = new_node;
 
 	return (new_node);
 }
