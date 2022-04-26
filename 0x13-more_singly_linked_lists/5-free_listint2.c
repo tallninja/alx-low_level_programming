2f5645cfcda962d9392997184714016d44edb7e0
tallninja <tallninja123@gmail.com>
2022-04-26 09:59:26 +0300

    5-free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - sets the head to NULL
+ * @head: head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next_node;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+}
