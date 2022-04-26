8124314f61d224199ef24a8b79870f63333665f1
tallninja <tallninja123@gmail.com>
2022-04-26 10:25:44 +0300

    6-pop_listint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,12 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *node;
-	int n;
 
 	if (head == NULL)
 		return (0);
 
 	node = *head;
 	*head = node->next;
-	n = node->n;
 
-	free(node);
-
-	return (n);
+	return (node->n);
 }
