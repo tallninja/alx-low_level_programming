35684e8f821d639fac6124924fad23b749896343
tallninja <tallninja123@gmail.com>
2022-04-28 09:37:13 +0300

    1-print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a binary representation of a number
+ * @n: number
+ */
+
+void print_binary(unsigned long int n)
+{
+	char *str = malloc(sizeof(n));
+	int i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (n)
+	{
+		if (n & 1)
+			str[i] = '1';
+		else
+			str[i] = '0';
+
+		n >>= 1;
+		i++;
+	}
+
+	for (i = strlen(str); i >= 0; i--)
+		_putchar(str[i]);
+}
