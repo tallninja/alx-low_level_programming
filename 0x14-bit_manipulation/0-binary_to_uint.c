3133524b9a5429f3dd6806f5710393556d7ced45
tallninja <tallninja123@gmail.com>
2022-04-28 09:09:57 +0300

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _pow - calculates a number raised by exponent
+ * @num: number
+ * @exp: exponent
+ * Return: number^exponent
+ */
+
+int _pow(int num, int exp)
+{
+	int result = 1;
+
+	if (exp == 0)
+		return (result);
+
+	while (exp > 0)
+		result *= num, exp--;
+
+	return (result);
+}
+
+/**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: binary number
  * Return: converted integer or 0 if failed
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,26 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/**
- * _pow - calculates a number raised by exponent
- * @num: number
- * @exp: exponent
- * Return: number^exponent
- */
-
-int _pow(int num, int exp)
-{
-    int result = 1;
-
-    if (exp == 0)
-        return (result);
-
-    while (exp > 0)
-        result *= num, exp--;
-
-    return (result);
-}
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 
