46cb4609120bfd00b203a8becc2773ba34c2090b
Ernest Wambua <ernestwambua2@gmail.com>
2022-05-03 13:57:45 +0300

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	if (text_content != NULL)
 	{
