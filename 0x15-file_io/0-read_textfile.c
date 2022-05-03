a981eeb6ace728519ac8eaee9aba11491d654ac6
Ernest Wambua <ernestwambua2@gmail.com>
2022-05-03 12:29:59 +0300

    0-read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: filename
+ * @letters: number of letters in the file
+ * Return: letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, letters_read, letters_wrote, buff_size = letters;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1) /* file could not be opened */
+		return (0);
+
+	buff = (char *) malloc(sizeof(char) * buff_size + 1);
+	if (buff == NULL)
+		return (0);
+
+	letters_read = read(fd, buff, buff_size);
+	buff[buff_size] = '\0';
+
+	letters_wrote = write(STDOUT_FILENO, buff, buff_size);
+	if (letters_wrote < buff_size - 1)
+		return (0);
+
+	close(fd); /* close the file upon completion */
+	return (letters_read);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
