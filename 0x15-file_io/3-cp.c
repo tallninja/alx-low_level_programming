7d86e9499da7c692314888d4fc2bb654c09ab673
Ernest Wambua <ernestwambua2@gmail.com>
2022-05-03 14:31:55 +0300

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	fd = open(argv[1], O_RDONLY); /* open file from */
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1])
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
