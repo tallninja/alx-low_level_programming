6784b3c98fff87e6bc1538dd570d6f893ff1e962
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-09 12:19:15 +0300

    4-free_dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: head
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	while (head)
+	{
+		head = head->next;
+		free(head);
+	}
+	free(head);
+}
