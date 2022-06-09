254ca2b6f5320fb94fc9a4a144385e8c226472c2
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-09 17:56:22 +0300

    4-free_dlistint

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,10 +8,13 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	while (head->next != NULL)
+	if (head != NULL)
 	{
-		head = head->next;
+		while (head->next != NULL)
+		{
+			head = head->next;
+			free(head);
+		}
 		free(head);
 	}
-	free(head);
 }
