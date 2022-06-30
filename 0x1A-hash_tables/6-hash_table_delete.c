4b4e05dee8168e0f90567676f45d40981677e561
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-30 10:31:25 +0300

    6-hash_table_delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ * Return: Nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *curr = NULL, *temp = NULL;
+	unsigned int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr != NULL)
+		{
+			temp = curr;
+			free(temp->key);
+			free(temp->value);
+			curr = curr->next;
+			free(temp);
+		}
+		free(curr);
+	}
+	free(ht->array);
+	free(ht);
+}
