8d30c9d5b12d24169bcd3780759dff27a940e0ed
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-30 09:35:06 +0300

    3-hash_table_set

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 
-	hash_table = malloc(sizeof(void *) * size);
+	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
 	return (hash_table);
 }
