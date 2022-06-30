426dbfd208cef1bbedf25033fdc47e8f45dc51bd
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-30 20:31:56 +0300

    3-hash_table_set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,6 +38,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(current_node->value);
 			current_node->value = strdup(value);
+			free(new_node->key);
+			free(new_node->value);
 			free(new_node);
 			return (1);
 		}
